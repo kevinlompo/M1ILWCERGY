73f8f56f2f8a2444a425482d1b7ad0b79f754804
kevinlompo <kevinlompo12@gmail.com>
2023-01-09 12:27:36 +0100

    permission gps

--- lib/services/permission_handler.dart
+++ lib/services/permission_handler.dart
@@ -12,18 +12,25 @@ class PermissionHandler {
     if(Platform.isAndroid){
       //Systeme android
       PermissionStatus storage = await Permission.storage.status;
+      PermissionStatus gps = await Permission.location.status;
 
       //Vérifier le statut pour le stockage
       checkStatusStorage(storage);
+
+      //Vérifier le statut du gps
+      checkStatusLocation(gps);
+
 
     }
     else
     {
       //Systeme ios
       PermissionStatus status = await Permission.photos.status;
+      PermissionStatus gps = await Permission.location.status;
 
       //Vérifier le status pour les photos
       checkStatusPhoto(status);
+      checkStatusLocation(gps);
     }
 
   }
@@ -38,13 +45,13 @@ class PermissionHandler {
 
 
     //cas réfusé
-      case PermissionStatus.denied : return await Permission.photos.request().then((value) => checkStatusLocation(value));
+      case PermissionStatus.denied : return await Permission.location.request().then((value) => checkStatusLocation(value));
     //cas seulement cette fois
 
 
-      case PermissionStatus.restricted : return await Permission.photos.request().then((value) => checkStatusLocation(value));
+      case PermissionStatus.restricted : return await Permission.location.request().then((value) => checkStatusLocation(value));
     //cas réussi
-      case PermissionStatus.granted : return await Permission.photos.request().then((value) => checkStatusLocation(value));
+      case PermissionStatus.granted : return await Permission.location.request().then((value) => checkStatusLocation(value));
       default : return Future.error("Erreur au niveau de la récupération du statuts");
     }
   }
@@ -59,13 +66,13 @@ class PermissionHandler {
 
 
     //cas réfusé
-      case PermissionStatus.denied : return await Permission.location.request().then((value) => checkStatusPhoto(value));
+      case PermissionStatus.denied : return await Permission.photos.request().then((value) => checkStatusPhoto(value));
     //cas seulement cette fois
 
 
-      case PermissionStatus.restricted : return await Permission.location.request().then((value) => checkStatusPhoto(value));
+      case PermissionStatus.restricted : return await Permission.photos.request().then((value) => checkStatusPhoto(value));
     //cas réussi
-      case PermissionStatus.granted : return await Permission.location.request().then((value) => checkStatusPhoto(value));
+      case PermissionStatus.granted : return await Permission.photos.request().then((value) => checkStatusPhoto(value));
       default : return Future.error("Erreur au niveau de la récupération du statuts");
     }
   }
